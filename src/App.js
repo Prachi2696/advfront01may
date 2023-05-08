347d95833a0c643a1b137bb098d71030f7ec625a
sagarnaik965 <sagarnaik965@gmail.com>
2023-05-08 13:46:29 +0530

    change by sagar in app.js

--- src/App.js
+++ src/App.js
@@ -340,7 +340,7 @@ function App() {
 
           {authStore.loginStatus &&
             <>
-              <div className="md:ml-64" style={{paddingTop:'8%'}}>
+              <div className="md:ml-64" style={{paddingTop:'12%'}}>
                 <Route exact path="/adv/applicationDetails" component={ApplicationDetails} />
                 <Route exact path="/adv/applicationinfo/:appcode" component={props => <ApplicationsInfo {...props} />} />
                 <Route exact path="/adv/appdetails" component={AppDetails} />
@@ -355,7 +355,7 @@ function App() {
 
       {!authStore.loginStatus &&
             <>
-              <div className="md:ml-64" style={{paddingTop:'8%'}}>
+              <div className="md:ml-64" style={{paddingTop:'12%'}}>
                 <Route exact path="/adv/LoginRequired" component={LoginRequired} />
                 <Route exact path="/adv/applicationinfo/:appcode" component={LoginRequired} />
                 <Route exact path="/adv/appdetails" component={LoginRequired} />
